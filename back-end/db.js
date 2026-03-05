f661f95ab3396092bb1d3a675600e1beaf2fe430
derekwu009 <dew009@ucsd.edu>
2026-03-04 21:13:15 -0800

    update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -3,10 +3,14 @@ import mariadb from "mariadb";
 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   connectionLimit: 5,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
